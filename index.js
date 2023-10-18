2dacd47cc885041c4b4aec5726d0bf78baa90801
RAJASHREE01 <srajashree2000@gmail.com>
2023-10-18 09:17:47 +0530

    front-end initial

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 3001;
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.render("index");
