f069f064b6f33f5d66181f8cbddbc7f751348115
RAJASHREE01 <srajashree2000@gmail.com>
2023-10-18 08:53:31 +0530

    routes added

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import express from "express";
-
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3001;
 
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index");
+})
+
+app.get("/about", (req, res) => {
+    res.render("about");
 })
 
 app.listen(port, () => {
