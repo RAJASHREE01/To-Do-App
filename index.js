2ccfbed2a3912daa67180fc15fef3d7b43a5acb6
RAJASHREE01 <srajashree2000@gmail.com>
2023-10-18 10:25:13 +0530

    added db

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose, { Schema } from "mongoose";
 
 const app = express();
 const port = 3001;
@@ -7,9 +8,37 @@ const port = 3001;
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+mongoose.connect("mongodb+srv://admin:hS4JfB9mrw9i1LYy@cluster0.lyg7lci.mongodb.net/?retryWrites=true&w=majority");
+
+const itemsSchema  = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item ({
+    name: "code"
+});
+
+const item2 = new Item({
+    name: "make pull request"
+});
+
+const defaultItems = [item1, item2]
+
+const listSchema = {
+    name: String,
+    items: [defaultItems]
+}
+
+// const List = mongoose.model("List", listSchema)
 
 app.get("/", (req, res) => {
-    res.render("index");
+    Item.find({}).then(function(foundItem){
+        console.log(foundItem)
+        
+        res.render("index", {listTitle: "Today", newListItems: foundItem});
+  })
 })
 
 app.get("/about", (req, res) => {
